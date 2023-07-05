f7423e7df9b41bd9581e1b5cc3c4f4bdfef195c6
Tushar Zalte <90615665+tusharzalte@users.noreply.github.com>
2023-07-05 21:44:14 +0530

    Update Profile.js

--- client1/src/Pages/Profile.js
+++ client1/src/Pages/Profile.js
@@ -15,7 +15,7 @@ function Profile() {
   const onFinish = async (values) => {
     setLoading(true);
     try {
-      const result = await axios.post("api/user/update", {
+      const result = await axios.post("https://backend1123.onrender.com/api/user/update", {
         ...values,
         _id: user._id,
       });
